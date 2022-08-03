26ee995cb929b22990e1a3e4dc024c19b33b5e6e
ALESSANDRO_XAVIER_SANTOS <12102253@OMEGAB23.BARROCA>
2022-08-03 11:55:45 -0300

    Adicionando mais telas dinamicas

---
+++ js/dynamowaves-main/README.md
@@ -0,0 +1,4 @@
+# Dynamowaves
+SVG wave HTML templates that shuffle their shape on each render!
+
+[Documentation + examples](https://dynamowaves.markzebley.com)
--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+/*------------------------------------------------------------------------------------------------------*/
+//?Barra de Navegação
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 750) { // 140 é a distancia que vc rola antes da logo sumir
+      $("#cabeca").css("width", "100%")
+      $("#cabeca").fadeOut();
+    } else {
+      $("#cabeca").css("width", "50%") 
+      $("#cabeca").fadeIn();
+    }
+  });
+
+/*------------------------------------------------------------------------------------------------------*/
 $(".links").on("click", function() {
     $(".line").hide();
     $(".links").removeClass("active");
